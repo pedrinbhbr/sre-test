7a5ed42f9bba08990e3f58b3af5df4a884471a87
pedrinbhbr <pedrinbhbr@gmail.com>
2024-09-08 16:38:49 -0300

    criando repo e subindo solucao

---
+++ dags/smooth.py
@@ -0,0 +1,20 @@
+from airflow.decorators import dag
+from airflow.operators.smooth import SmoothOperator
+from datetime import datetime
+
+
+@dag(
+    dag_id="yt-video",
+    schedule=None,
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=["smooth"]
+)
+def smooth():
+    video = SmoothOperator(
+        task_id="youtube_video"
+    )
+
+
+
+smooth()
